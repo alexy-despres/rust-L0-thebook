a29ac31fdb083c7447a82771f86da53781c6a105
Alexy Despres <alexydespres.dev@gmail.com>
2026-01-04 23:24:18 -0500

    feat(ch03): demonstrate const vs let usage

--- 3_common_concepts/variables/src/main.rs
+++ 3_common_concepts/variables/src/main.rs
@@ -1,6 +1,10 @@
+const TWO: u8 = 1 + 1;
+// let y = 7; <- This would not work as you can't declare a variable outside of a function
+
 fn main() {
     let mut x = 5; // Here we use the mut keyword to make it mutable.
     println!("The value of x is: {x}");
     x = 6; // Without the use of the mut keyword, we wouldn't have been able to do this.
     println!("The value of x is: {x}");
+    println!("1 + 1 = {TWO}")
 }
