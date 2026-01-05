d4a7dd0fe3e806b3ac7e2b30915bc74fcd252800
Alexy Despres <alexydespres.dev@gmail.com>
2026-01-04 23:09:56 -0500

    feat(ch03): demonstrate variable mutability

---
+++ 3_common_concepts/variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut x = 5; // Here we use the mut keyword to make it mutable.
+    println!("The value of x is: {x}");
+    x = 6; // Without the use of the mut keyword, we wouldn't have been able to do this.
+    println!("The value of x is: {x}");
+}
